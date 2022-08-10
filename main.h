82b270aa77528ec96b8249ca08e10f3bda576cfe
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-08-10 12:17:30 +0300

    a function that produces output according to a format.

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdargs.h>
+
+int _putchar(char c);
+int printf(const char *format, ...);
+int print_string(char *);
+
+#endif
